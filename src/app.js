8f0c166470ca1c8ad253a231df034fd42ff6297f
AlyssaBalaoro <balaoroalyssa@gmail.com>
2022-12-29 14:22:19 +0800

    Install hbs and request

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const hbs = require("hbs");
+const path = require("path");
 const app = express();
+
 
 const port = process.env.PORT || 3000
 
+
+app.get('', (req, res) => {
+    res.send("Hi this is a weather app");
+})
+app.get('/weather', (req, res) => {
+    res.send("This is weather endpoint.")
+})
+app.get("*", (req, res) => {
+    res.send("Page not found.")
+})
 app.listen(port, () => {
     console.log("Server is up and running on port: ", port);
 })
