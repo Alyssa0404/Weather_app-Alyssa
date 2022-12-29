6749ca8fb4a7358d4e8dff86d92f3da89a39e536
AlyssaBalaoro <balaoroalyssa@gmail.com>
2022-12-29 16:40:35 +0800

    Added files

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "ec949209676f27f383a1429b24f0d830"
+    }
+}
+
+module.exports = constants;
